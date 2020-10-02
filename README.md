db615b5ccc1ebf4cb1120b8a13e394862a0c00b7
Curtis Deptuck <curtdept@me.com>
2020-10-02 13:09:15 -0600

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# choco-stern
+
+This repository is the source for the packaging around [Stern](https://github.com/curtdept/choco-stern).
+
+The source and releases for Stern are located on their [github page](https://github.com/wercker/stern).
+
+If there are any issues please file them here. 
+I will try to attend to them ASAP.
+
+If a new version is needed, right now it's a manual update for me to support.
+I will eventually be looking at automatic packaging in the near future.
---
+++ src/tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
+$version = '1.11.0'
+
+$exe_name = "stern_windows_amd64.exe"
+
+# only 64bit url
+$url = "https://github.com/wercker/stern/releases/download/$version/stern_windows_amd64.exe"
+
+# use $ checksum [exe] -t=sha256
+$exe_checksum = '75708B9ACF6EF0EEFFBE1F189402ADC0405F1402E6B764F1F5152CA288E3109E'
+$checksum_type = 'sha256'
+
+# destinations
+$exeLocation = join-path $toolsDir $exe_name
+
+$getArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = $exeLocation
+  url64bit      = $url
+  checksum64    = $exe_checksum
+  checksumType64= $checksum_type
+}
+
+Get-ChocolateyWebFile @getArgs
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'stern*'
+  fileType      = 'exe'
+  silentArgs    = ""
+  validExitCodes= @(0)
+  file64        = $exeLocation
+  checksum64    = $exe_checksum
+  checksumType64= $checksum_type
+  destination   = $toolsDir
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+$binargs = @{
+  name = 'stern'
+  path = $exeLocation
+}
+
+Install-BinFile @binArgs
