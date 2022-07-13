55829e81be34406a01fbcc5ce1da6c1b2959b8aa
Curtis Deptuck <curtdept@me.com>
2022-07-13 10:37:17 -0600

    1.21

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -2,35 +2,54 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # release version of kustomize: https://github.com/kubernetes-sigs/kustomize/releases
-$version = '1.11.0'
-
-$exe_name = "stern_windows_amd64.exe"
+$version = '1.21.0'
+# pattern for archive name
+$base_name = "stern_$($version)_windows_amd64"
+$zip_name = $base_name + ".tar.gz"
+$tar_name = $base_name + ".tar"
+$exe_name = "stern.exe"
 
 # only 64bit url
-$url = "https://github.com/wercker/stern/releases/download/$version/stern_windows_amd64.exe"
+$url = "https://github.com/stern/stern/releases/download/v$($version)/$($zip_name)"
 
 # use $ checksum [exe] -t=sha256
-$exe_checksum = '75708B9ACF6EF0EEFFBE1F189402ADC0405F1402E6B764F1F5152CA288E3109E'
+$archive_checksum = 'C3F148576E604B250F4DF4769079F7B69F62B2DFCDFF10DC5FBD51815C297331'
+$exe_checksum = '2829D2B6C1B679EE4613D9285307803F99E882A9AD2101DE1CAD7574C7F7419F'
 $checksum_type = 'sha256'
 
 # destinations
+$zipLocation = join-path $toolsDir $zip_name
 $exeLocation = join-path $toolsDir $exe_name
 
 $getArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileFullPath  = $exeLocation
+  fileFullPath  = $zipLocation
   url64bit      = $url
-  checksum64    = $exe_checksum
+  checksum64    = $archive_checksum
   checksumType64= $checksum_type
 }
 
 Get-ChocolateyWebFile @getArgs
 
+$unzipArgs = @{
+  fileFullPath = $zipLocation
+  destination = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
+
+$unzip2Args = @{
+  fileFullPath = join-path $toolsDir $tar_name
+  destination = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzip2Args
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'stern*'
+  softwareName  = 'stern'
   fileType      = 'exe'
-  silentArgs    = ""
+  silentArgs    = "--version"
   validExitCodes= @(0)
   file64        = $exeLocation
   checksum64    = $exe_checksum
